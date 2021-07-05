8b9fc56b95b264734c4e579748e86e6e3f87de3b
Chandrashekar112 <chandrashekar.sukkali@ness.com>
2021-07-05 15:24:01 +0530

    'siginchnages'

--- Backend/src/routers/order.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-const { getOrders } = require("../controllers/orderController");
-
-router.get("/", getOrders);
-
-module.exports = router;
